1653c9b327d38027fc7d6b731ae79bb3e60b8f2e
lvjinke <lvjinkebit@163.com>
2025-03-10 18:10:37 +0800

    chapter09 updated

---
+++ car.py
@@ -0,0 +1,25 @@
+class Car():
+    """A simple attempt to represent a car."""
+
+    def __init__(self, make, model, year):
+        """Initial atributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+
+
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+
+    def update_odometer(self, mileage):
+        """Set the odometer reading to the given value."""
+        self.odometer_reading = mileage
